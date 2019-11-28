2d6b5a5dd3933edef72a3e9bc2102d0f40dd8e8b
takuya.fujihara <tayatengyuan0@gmail.com>
2019-11-28 15:54:08 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by 藤原拓也 on 2019/11/28.
+//  Copyright © 2019 takuya.fujihara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
